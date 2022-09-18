ad29b8ac9b775991bd335021450271ec37bee41a
AmonMcDuul <joranomic@hotmail.com>
2022-09-18 19:31:00 +0200

    store beginnings...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,25 +12,37 @@ import { WeatherForecastService } from './services/weather-forecast.service';
 
 
 import { SubtractingComponent } from './subtracting/subtracting.component';
+import { StoreModule } from '@ngrx/store';
+import { NotesComponent } from './notes/notes.component';
+import { notesReducer } from './state/notes.reducer';
+import { collectionReducer } from './state/collection.reducer';
+import { NotesCollectionComponent } from './notes-collection/notes-collection.component';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { NotesService } from './services/notes.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     TestComponent,
-    SubtractingComponent
+    SubtractingComponent,
+    NotesComponent,
+    NotesCollectionComponent,
+    
   ],
   imports: [
     BrowserModule,
-
     HttpClientModule,
     AppRoutingModule,
     RouterModule.forRoot([
       {path: 'subtracting', component: SubtractingComponent},
       {path: 'test', component: TestComponent},
     ]),
+    StoreModule.forRoot({ notes: notesReducer, collection: collectionReducer }),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production, }),
   ],
-  providers: [WeatherForecastService],
+  //providers: [WeatherForecastService, NotesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
